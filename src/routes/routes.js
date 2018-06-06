6d18b04d85697d8f2f2cc158cd0eb791e09fe591
Sergey Chernov <s.chernov@bankex.com>
2018-06-06 17:20:31 +0300

    endpoints can be post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,25 @@
 const render = require('../render');
+
 var appRouter = function (app) {
 	const fs = require('fs');
 	const services = JSON.parse(fs.readFileSync(__dirname+'/routes.json', 'utf8'));
+	const addEndPoint = (type, path)=>{
+		switch(type){
+			case 'get':{
+				console.log(type, path);
+				app.get(path, function (req, res) {
+					const data = services[type][req.route.path];
+					res.status(200).send(data);
+				});
+			}break;
+			case 'post':{
+				app.post(path, function (req, res) {
+					const data = services[req.route.path];
+					res.status(200).send(data);
+				});
+			}break;
+		}
+	};
 	app.get("/", function(req, res) {
 		res.status(200).send("Welcome to our restful API");
 	});
@@ -11,33 +29,34 @@ var appRouter = function (app) {
 	app.post("/updateservice", function(req,res){
 		if(req.body.services){
 			const newServices = JSON.parse(req.body.services);
-			for (var key in newServices) {
-				if (newServices.hasOwnProperty(key)) {
-					if(!services.hasOwnProperty(key)) {
-						app.get(key, function (req, res) {
-							const data = services[key];
-							res.status(200).send(data);
-						});
+			for (var type in newServices) {
+				if (newServices.hasOwnProperty(type)) {
+					if(!services.hasOwnProperty(type)){
+						newServices[type]={};
 					}
-					services[key] = newServices[key];
+					for(path in newServices[type]){
+						if (newServices[type].hasOwnProperty(path)) {
+							addEndPoint(type,path);
+							services[type][key] = newServices[type][key];
+						}
 				}
+				}
+
 			}
 		}
+
 		fs.writeFileSync(__dirname+'/routes.json', JSON.stringify(services, null, 2) , 'utf-8');
 		res.status(200).send(render.changeservices({json:JSON.stringify(services,null, 2)}));
 	});
-	for (var key in services) {
-		if (services.hasOwnProperty(key)) {
-			console.log('service:',key);
-			app.get(key, function (req, res) {
-				const data = services[req.route.path];
-				res.status(200).send(data);
-				console.log('service:',req.route.path);
-			});
-
+	for (var type in services) {
+		if(services.hasOwnProperty(type)){
+			for(path in services[type]){
+				if (services[type].hasOwnProperty(path)) {
+					addEndPoint(type, path);
+				}
+			}
 		}
 	}
-
 }
 
 module.exports = appRouter;
