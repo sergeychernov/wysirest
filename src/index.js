b4bdde406e021416795f8db9017777b9c89dfb0d
Sergey Chernov <s.chernov@bankex.com>
2018-06-09 10:30:14 +0300

    add cors authorization and some others headers

--- src/index.js
+++ src/index.js
@@ -8,14 +8,19 @@ require("dot").process({
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
-var index = express();
+var app = express();
 
 
-index.use(bodyParser.json());
-index.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+	res.append('Access-Control-Allow-Origin', ['*']);
+	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.append('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+	next();
+});
+routes(app);
 
-routes(index);
-
-var server = index.listen(3333, function () {
+var server = app.listen(3333, function () {
 	console.log("index running on port.", server.address().port);
 });
